38eaefeca7f997693802e13f479b9c68b8f9ad8b
milka1g <nikson@gmail.com>
2020-06-26 14:37:45 +0200

    nmg

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 function setLanguage(){
     
 }
+
+function ugliraj(){
+    tags = document.getElementsByClassName("ugl");
+    pL = 10;
+    
+    for(var i = 0; i<tags.length; i++){
+        pL+=15;
+        tags[i].style.paddingLeft = pL+"px";
+    }
+
+    tags = document.getElementsByClassName("ugl1");
+    pL = 0;
+    
+    for(var i = 0; i<tags.length; i++){
+        pL+=30;
+        tags[i].style.paddingLeft = pL+"px";
+    }
+
+    tags = document.getElementsByClassName("ugl2");
+    pL = 0;
+    
+    for(var i = 0; i<tags.length; i++){
+        pL+=20;
+        tags[i].style.paddingLeft = pL+"px";
+    }
+}
+
+

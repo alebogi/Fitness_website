567f3ba98c3332a0b4e71f649cb4f92d43d63ba7
milka1g <nikson@gmail.com>
2020-06-27 15:01:27 +0200

    pojedinacni i brcr

--- js/script.js
+++ js/script.js
@@ -5,26 +5,36 @@ function setLanguage(){
 function ugliraj(){
     tags = document.getElementsByClassName("ugl");
     pL = 10;
+    var incr = window.innerWidth;
+    incr = incr/200;
     
     for(var i = 0; i<tags.length; i++){
-        pL+=15;
+        pL+=incr+2;
         tags[i].style.paddingLeft = pL+"px";
     }
 
     tags = document.getElementsByClassName("ugl1");
-    pL = 0;
+    pL = 10;
     
     for(var i = 0; i<tags.length; i++){
-        pL+=30;
+        pL+=(incr*2 + 5);
         tags[i].style.paddingLeft = pL+"px";
     }
 
     tags = document.getElementsByClassName("ugl2");
-    pL = 0;
+    pL = 10;
     
     for(var i = 0; i<tags.length; i++){
-        pL+=20;
+        pL+=(incr*3+ 5);
         tags[i].style.paddingLeft = pL+"px";
+    }
+
+    tags = document.getElementsByClassName("naslov");
+    var naslov = window.innerWidth;
+    naslov = naslov/90;
+    
+    for(var i = 0; i<tags.length; i++){
+        tags[i].style.paddingLeft = naslov+"px";
     }
 }
 
